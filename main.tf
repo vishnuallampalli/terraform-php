1de29d01f825e8b056b1c83ecb6da966db0d64b5
Karan8808 <122865824+Karan8808@users.noreply.github.com>
2023-04-19 16:53:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ locals {
   vpc_id           = "vpc-01bd8dfbca63589e0"
   subnet_id        = "subnet-0fff856dcab65951c"
   ssh_user         = "ec2-user"
-  key_name         = "password"
+  key_name         = "new-key"
   private_key_path = "/home/ec2-user/new-key.pem"
 }
 
